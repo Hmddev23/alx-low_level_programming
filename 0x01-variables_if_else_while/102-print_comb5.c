6b59bffa28dd929354abd3d87a1711ccf32e2afb
Abdelhamid <hamid.maira709@gmail.com>
2023-06-17 21:05:03 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,5 +11,21 @@
 int main(void)
 {
 	int nbr1, nbr2;
+
+	for (nbr1 = 0; nbr1 <= 9; ++nbr1)
+	{
+		for (nbr2 = nbr1 + 1; nbr2 <= 9; ++nbr2)
+		{
+			putchar('0' + nbr1);
+			putchar('0' + nbr2);
+			if (nbr1 != 7 || nbr2 != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
