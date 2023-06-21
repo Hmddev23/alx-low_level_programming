e0bf456df0fc17b983db38d5d8b0b798c140ba56
Abdelhamid <hamid.maira709@gmail.com>
2023-06-21 09:50:26 -0700

    jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
  *
  * jack_bauer - check the code.
  *
- * Description: print every minute of the day of Jack Bauer
+ * jack_bauer: print every minute of the day of Jack Bauer
  *		starting from 00:00 to 23:59.
  *
  * Return: Nothing.
