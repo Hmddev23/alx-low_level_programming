689b481ef87f2903c4ac654cc0dbea2ad6befa42
Abdelhamid <hamid.maira709@gmail.com>
2023-06-26 19:49:24 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ *
+ * @s: String parameter to be converted.
+ *
+ * Return: Converted integer.
+ */
+
+int _atoi(char *s)
+{
+	return 0;
+}
