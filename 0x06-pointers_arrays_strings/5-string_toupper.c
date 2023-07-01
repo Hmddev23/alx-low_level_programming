78a5f1ffca462a963661c115f7ea39fca7756780
Abdelhamid <hamid.maira709@gmail.com>
2023-06-30 18:24:08 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * string_toupper - Change all lowercase letters to uppercase.
+	*
+  * @s: String parameter to modify.
+  *
+  * Return: Resulting string.
+  *
+  */
+
+char *string_toupper(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] >= 'a' && s[index] <= 'z')
+		{
+			s[index] = s[index] - 32;
+		}
+	}
+	return (s);
+}
