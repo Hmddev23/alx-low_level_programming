28b618ed7960aa81d87b8e66c2c11ce9d1e8cc3c
Abdelhamid <hamid.maira709@gmail.com>
2023-07-03 18:37:55 -0700

    string char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strchr - Get the length of a prefix substring.
+  *
+  * @s: String to search.
+  * @c: Char to find.
+  *
+  * Return: Number of bytes in the initial segment of s.
+  */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return NULL;
+}
