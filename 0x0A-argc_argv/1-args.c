d2e7b9aeb9c81784c425989a85ff8b80bcbd6beb
Abdelhamid <hamid.maira709@gmail.com>
2023-07-07 15:49:40 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - Entry point of the program.
+  *
+  * @argc: The number of command-line arguments.
+  * @argv: An array of strings containing the command-line arguments.
+  *
+  * Return: Always 0 (success).
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
