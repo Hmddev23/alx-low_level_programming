04230999fa6f0dfa388e4cfc50e819df3edde940
Abdelhamid <hamid.maira709@gmail.com>
2023-08-01 17:44:15 -0700

    print list sage

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,7 +10,46 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+	size_t count = 0;
 
-	return (0);
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+		count++;
+
+		if (slow == fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			break;
+		}
+	}
+
+	if (slow == fast)
+	{
+		printf("-> [%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		count++;
+
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			count++;
+		}
+	}
+	else
+	{
+		while (slow != NULL)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			count++;
+		}
+	}
+
+	return (count);
 }
