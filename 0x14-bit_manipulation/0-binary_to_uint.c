0e2a92d4e61e870a17dd8fb5ce06cfb11bc8d821
Hmddev23 <hamid.maira709@gmail.com>
2023-09-01 01:32:36 +0100

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
-  * convert_binary_to_uint - Convert a binary number to an unsigned int.
+  * binary_to_uint - Convert a binary number to an unsigned int.
   *
-  * @binary_string: Pointer to a string of 0 and 1 chars.
+  * @b: Pointer to a string of 0 and 1 chars.
   *
   * Return: Converted number, '0' or '1' if it fails.
   */
 
-unsigned int convert_binary_to_uint(const char *binary_string)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int bit;
+        unsigned int result = 0;
+        int bit;
 
-	if (binary_string == NULL)
-		return (0);
+        if (b == NULL)
+                return (0);
 
-	while (*binary_string != '\0')
-	{
-	bit = *binary_string - '0';
+        while (*b != '\0')
+        {
+                bit = *b - '0';
 
-		if (bit != 0 && bit != 1)
-			return (0);
+                if (bit != 0 && bit != 1)
+                        return (0);
 
-		result = (result << 1) | bit;
-		binary_string++;
-	}
+                result = (result << 1) | bit;
+                b++;
+        }
 
-	return (result);
+        return (result);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned int convert_binary_to_uint(const char *binary_string);
-void print_binary_representation(unsigned long int number);
-int get_bit_at_index(unsigned long int number, unsigned int index);
-int set_bit_at_index(unsigned long int *number, unsigned int index);
-int clear_bit_at_index(unsigned long int *number, unsigned int index);
-unsigned int count_different_bits(unsigned long int num1, unsigned long int num2);
-int check_endianness(void);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 
 #endif /* MAIN_H */
